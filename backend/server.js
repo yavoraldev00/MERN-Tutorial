2104d5e1cd03d805e598fab1b9a826c58c3039ec
YAVOR ALDEV <y.aldev@student.fontys.nl>
2024-07-20 14:45:37 +0200

    tutorial 3 - epxress router and api routes (Uppercase routes folder fixed issue)

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,23 @@
-require("dotenv").config();
+require('dotenv').config()
 
-const express = require("express");
+const express = require('express')
+const workoutRoutes = require('./Routes/workouts')
 
 // express app
-const app = express();
+const app = express()
 
 // middleware
-app.use((req,res,next) => {
-    console.log(req.path, req.method)
-    next();
+app.use(express.json())
+
+app.use((req, res, next) => {
+  console.log(req.path, req.method)
+  next()
 })
 
 // routes
-app.get("/", (req, res) => {
-    res.json({mssg: "Here is a response object!"})
-})
+app.use('/api/workouts', workoutRoutes)
 
-// listening for requests
-app.listen(process.env.PORT, ()=> {
-    console.log("Listening to port 3000...")
-});
+// listen for requests
+app.listen(process.env.PORT, () => {
+  console.log('listening on port', process.env.PORT)
+})
