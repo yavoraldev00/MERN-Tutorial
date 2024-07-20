565097fef2ef6a6d8db51edd4285089e93161ee4
YAVOR ALDEV <y.aldev@student.fontys.nl>
2024-07-20 15:45:50 +0200

    tutorial 4 - MongoDB connection

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 
 const express = require('express')
+const mongoose = require('mongoose')
 const workoutRoutes = require('./Routes/workouts')
 
 // express app
@@ -9,6 +10,18 @@ const app = express()
 // middleware
 app.use(express.json())
 
+// connect to db
+mongoose.connect(process.env.MONG_URI)
+  .then(() => {
+    // listen for requests
+    app.listen(process.env.PORT, () => {
+      console.log('Connected to database! Listening on port', process.env.PORT)
+    })
+  })
+  .catch(error => {
+    console.log(error)
+  })
+
 app.use((req, res, next) => {
   console.log(req.path, req.method)
   next()
@@ -16,8 +29,3 @@ app.use((req, res, next) => {
 
 // routes
 app.use('/api/workouts', workoutRoutes)
-
-// listen for requests
-app.listen(process.env.PORT, () => {
-  console.log('listening on port', process.env.PORT)
-})
