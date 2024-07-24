c533be43e8d758bfdc594ad01280454a8b666cae
YAVOR ALDEV <y.aldev@student.fontys.nl>
2024-07-24 20:36:33 +0200

    delete requests

--- frontend/src/components/WorkoutDetails.js
+++ frontend/src/components/WorkoutDetails.js
@@ -1,4 +1,21 @@
+import { useWorkoutsContext } from "../hooks/useWorkoutsContext";
+
+
 const WorkoutDetails = ({ workout }) => {
+    const { dispatch } = useWorkoutsContext();
+
+    const handleDelete = async () => {
+        const response = await fetch("http://localhost:4000/api/workouts/" + workout._id, {
+            method: "DELETE"
+        })
+
+        const json = await response.json()
+
+        if(response.ok){
+            dispatch({type: "DELETE_WORKOUT", payload: json})
+        }
+    }
+
     return ( 
         <div className="workout-details">
             <h4>{workout.title}</h4>
@@ -6,6 +23,7 @@ const WorkoutDetails = ({ workout }) => {
             <p><strong>Reps: {workout.reps}</strong></p>
             <p>Created at: {workout.createdAt}</p>
             <p>id: {workout._id}</p>
+            <span onClick={handleDelete}>Delete</span>
         </div>
      );
 }
