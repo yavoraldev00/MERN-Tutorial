c1fc30343aaa15a122a5126aa30f758c04157073
YAVOR ALDEV <y.aldev@student.fontys.nl>
2024-07-25 15:44:20 +0200

    auth tutorial 6 -signing tokens

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,10 @@
 const User = require("../models/userModel");
+const jwt = require("jsonwebtoken");
+
+// generate json token function
+const createToken = (_id) => {
+    return jwt.sign({_id},process.env.SECRET,{ expiresIn: "1d" })
+}
 
 //  login
 const loginUser = async (req, res) => {
@@ -12,7 +18,11 @@ const registerUser = async (req, res) => {
     try{
         const user = await User.signup(email, password)
 
-        res.status(200).json({email, user})
+        debugger;
+        // creates a token
+        const token = createToken(user._id)
+
+        res.status(200).json({email, token})
     }catch(error){
         res.status(400).json({error: error.message})
     }
