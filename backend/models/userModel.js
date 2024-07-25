87292b943b4d62da4668e07fa28c9edddaa4e2bf
YAVOR ALDEV <y.aldev@student.fontys.nl>
2024-07-25 13:04:35 +0200

    auth tutorial 3 - sign up method and hashing password

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const Schema = mongoose.Schema;
 
@@ -9,9 +10,28 @@ const userSchema = new Schema({
         unique: true
     },
     password: {
-        type: Number,
+        type: String,
         required: true
     }
 });
+
+// static signup method
+userSchema.statics.signup = async function (email, password){
+
+    const exists = await this.findOne({ email })
+
+    if (exists) {
+        throw Error("Email already in use")
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(password, salt);
+
+    debugger;
+
+    const user = await this.create({ email, password: hash})
+
+    return user
+}
 
 module.exports = mongoose.model("User", userSchema);
