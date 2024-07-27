30ffef62bd8cacebf2806ace7fd8f12d5cddfd1b
YAVOR ALDEV <y.aldev@student.fontys.nl>
2024-07-28 01:41:09 +0200

    auth tutorial 14 - protecting API routes (error when not logged in at home)

--- backend/Routes/workouts.js
+++ backend/Routes/workouts.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const { createWorkout, getWorkouts, getWorkout, deleteWorkout, updateWorkout } = require('../controllers/workoutController')
+const requireAuth = require("../middleware/requireAuth")
+
 const router = express.Router()
+
+router.use(requireAuth)
 
 // GET all workouts
 router.get('/', getWorkouts)
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,5 +1,7 @@
 import { useEffect } from "react";
 import { useWorkoutsContext } from "../hooks/useWorkoutsContext";
+import { useAuthContext } from "../hooks/useAuthContext";
+
 
 // components
 import WorkoutDetails from "../components/WorkoutDetails";
@@ -7,11 +9,14 @@ import WorkoutForm from "../components/WorkoutForm";
 
 const Home = () => {
     const {dispatch, workouts} = useWorkoutsContext();
+    // const {user} = useAuthContext();
 
     useEffect(() => {
         const fetchWorkouts = async () => {
-            const response = await fetch("http://localhost:4000/api/workouts")
+            const user = JSON.parse(localStorage.getItem('user'))
+            const token = user.token;
 
+            const response = await fetch("http://localhost:4000/api/workouts", {headers: {authorization: `${token}`}})
             const json = await response.json()
 
             if(response.ok){
